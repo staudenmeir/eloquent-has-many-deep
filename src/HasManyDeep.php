a1f549bc13d16104a2cbde2ff823bbf20a672b1e
ahoiroman <roman.lossin@icloud.com>
2021-12-07 12:54:43 +0100

    Mark HasManyDeep as generic model (#147)
    * Mark HasManyDeep as generic model
    
    Check https://github.com/nunomaduro/larastan/blob/19866e06d5846f8c17460ce1c1808da8166d3747/UPGRADE.md#generic-relations for details.
    
    * Apply suggestions from code review
    
    Co-authored-by: Jonas Staudenmeir <mail@jonas-staudenmeir.de>
    
    Co-authored-by: Jonas Staudenmeir <mail@jonas-staudenmeir.de>

--- src/HasManyDeep.php
+++ src/HasManyDeep.php
@@ -11,6 +11,10 @@ use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Pagination\CursorPaginator;
 use Illuminate\Support\Collection;
 
+/**
+ * @template TRelatedModel of \Illuminate\Database\Eloquent\Model
+ * @extends \Illuminate\Database\Eloquent\Relations\Relation<TRelatedModel>
+ */
 class HasManyDeep extends HasManyThrough
 {
     use HasEagerLimit;
