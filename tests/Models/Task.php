8ad2a46b55a6eefa74a2e057531f1bb14bc48599
Jonas Staudenmeir <mail@jonas-staudenmeir.de>
2023-02-05 10:40:16 +0100

    Restore awobaz/compoships tests after support for Laravel 10

--- tests/Models/Task.php
+++ tests/Models/Task.php
@@ -8,7 +8,7 @@ use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Task extends Model
 {
-    //use Compoships; TODO[L10]
+    use Compoships;
 
     public function employee(): ComposhipsBelongsTo
     {
